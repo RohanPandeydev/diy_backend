0e6f020e3549e661b3147c40fbc94a9443146c9f
Rohan <rohan.kr.pandey2.0@gmail.com>
2025-05-05 23:48:05 +0530

    change

--- src/models/index.js
+++ src/models/index.js
@@ -9,14 +9,7 @@ const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../../config/config.js")[env];
 const db = {};
 
-const sequelize = new Sequelize(process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    port: process.env.DB_PORT || 3306,
-  });
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
 (async function () {
   try {
     await sequelize.authenticate();
